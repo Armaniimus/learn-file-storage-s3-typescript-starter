8ca0c1634cff868cc07ca1d602ea25811cf9741e
Armaniimus@laptop <tommy_chester@hotmail.com>
2026-05-25 22:37:15 +0200

    video cdn deployed

--- src/api/video-meta.ts
+++ src/api/video-meta.ts
@@ -4,6 +4,8 @@ import { createVideo, deleteVideo, getVideo, getVideos } from "../db/videos";
 import { respondWithJSON } from "./json";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 import type { BunRequest } from "bun";
+import { dbVideoToSignedVideo } from "./videos";
+import { type Video } from "../db/videos";
 
 export async function handlerVideoMetaCreate(cfg: ApiConfig, req: Request) {
   const token = getBearerToken(req.headers);
@@ -51,8 +53,14 @@ export async function handlerVideoGet(cfg: ApiConfig, req: BunRequest) {
   }
 
   const video = getVideo(cfg.db, videoId);
+
   if (!video) {
     throw new NotFoundError("Couldn't find video");
+  }
+
+  const presignedVideo = await dbVideoToSignedVideo(cfg, video);
+  if (presignedVideo.videoURL == undefined) {
+    throw Error("presignedVideo.videoURL == undefined")
   }
 
   return respondWithJSON(200, video);
@@ -63,5 +71,15 @@ export async function handlerVideosRetrieve(cfg: ApiConfig, req: Request) {
   const userID = validateJWT(token, cfg.jwtSecret);
 
   const videos = getVideos(cfg.db, userID);
-  return respondWithJSON(200, videos);
+
+  if (!videos) {
+    throw new NotFoundError("Couldn't find videos");
+  }
+
+  const presignedVideos: Video[] = []
+  for (const vid of videos) {
+    presignedVideos.push(await dbVideoToSignedVideo(cfg, vid));
+  }
+
+  return respondWithJSON(200, presignedVideos);
 }
--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,125 @@
 import { respondWithJSON } from "./json";
+import { getBearerToken, validateJWT } from "../auth";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { deleteVideo, getVideo, updateVideo } from "../db/videos";
+import { randomBytes } from "crypto";
 
 import { type ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { s3, S3Client, type BunRequest } from "bun";
+import { unlink } from "fs/promises";
+import { type Video } from "../db/videos";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  const videoMeta = await getVideo(cfg.db, videoId);
+  if (videoMeta == undefined) {
+    throw new BadRequestError("video does not exist");
+
+  } else if (videoMeta.userID !== userID) {
+    throw new UserForbiddenError("you cant upload thumbnail to another persons thumbnail");
+  }
+
+  console.log("uploading video", videoId, "by user", userID);
+
+  const formdata = await req.formData();
+  const videoFormData = formdata.get("video");
+  if (!(videoFormData instanceof File)) {
+    throw new BadRequestError("video is not an instance of file");
+  } 
+
+  const MAX_UPLOAD_SIZE = 1 << 30;
+  if (videoFormData.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("video is greater then 1gb");
+  }
+    
+  const isVideo = (videoFormData.type == "video/mp4");
+  if (!isVideo) {
+    throw new BadRequestError("video isn't an video mime type");
+  }
+  
+  const arrayBuffer = await videoFormData.arrayBuffer()
+  const filename = randomBytes(32).toString("base64url") + "." +videoFormData.type.split("/")[1];
+  const fileUrl = `${cfg.assetsRoot}/${filename}`;  
+  await Bun.write(fileUrl, arrayBuffer);  
+  const processedFile = await processVideoForFastStart(fileUrl);
+
+  try {
+    const prefix = await getVideoAspectRatio(processedFile);
+    videoMeta.videoURL = `${prefix}/${filename}`;
+
+    const presignedVideo = await dbVideoToSignedVideo(cfg, videoMeta);
+    if (presignedVideo.videoURL == undefined) {
+      throw Error("presignedVideo.videoURL == undefined")
+    }
+
+    const s3File = await cfg.s3Client.file(presignedVideo.videoURL)
+    await s3File.write(Bun.file(processedFile), { type: videoFormData.type });
+    await updateVideo(cfg.db, videoMeta);
+  } finally {
+    unlink(fileUrl);
+    unlink(processedFile);
+  } 
+  
   return respondWithJSON(200, null);
+}
+async function getVideoAspectRatio(filePath: string) {
+  const file = await Bun.spawn({
+    cmd: ["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "json", filePath],
+    stderr: "pipe",
+    stdout: "pipe"
+  });
+
+  if (await file.exited != 0) {
+    const stderrText = await new Response(file.stderr).text();
+    console.error(stderrText);
+  } else {
+    const stdoutJson = await new Response(file.stdout).json();
+    const width = stdoutJson.streams[0].width;
+    const height = stdoutJson.streams[0].height;
+
+    const isLandscape = () => Math.floor(width / 16) == Math.floor(height / 9);
+    const isPortrait = () => Math.floor(width / 9) == Math.floor(height / 16);
+
+    if (isLandscape()) {
+      return "landscape"
+    } else if (isPortrait()) {
+      return "portrait"
+    }
+    return "other"
+  }
+}
+
+async function processVideoForFastStart(inputFilePath:string) {
+  const outputPath = inputFilePath + ".processed"
+  const command = await Bun.spawn({
+    cmd: ["ffmpeg", "-i", inputFilePath, "-movflags", "faststart", "-map_metadata", "0", "-codec", "copy", "-f", "mp4", outputPath],
+  });
+  const result = await command.exited
+  console.log(result);
+  
+  return outputPath;
+}
+
+async function generatePresignedURL(cfg: ApiConfig, key: string, expireTime: number) {
+  return await cfg.s3Client.presign(key, {
+    expiresIn: expireTime,
+    method: "PUT",
+    type: "video/mp4",
+    bucket: cfg.s3Bucket
+  });
+}
+
+export async function dbVideoToSignedVideo(cfg: ApiConfig, video: Video) {
+  if (video.videoURL == undefined) {
+    throw Error("video.videoURL == undefined")
+  }
+  video.videoURL = await generatePresignedURL(cfg, video.videoURL, 3600)
+  return video;
 }
